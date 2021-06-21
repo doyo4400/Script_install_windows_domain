745c0069bf788a2fc7119213f00618f48af6a3f7
lolo35740 <40354933+lolo35740@users.noreply.github.com>
2021-06-21 17:11:55 +0200

    Update dhcp.ps1

--- dhcp.ps1
+++ dhcp.ps1
@@ -1,7 +1,14 @@
+#install role
+Install-windowsfeature -name DHCP -IncludeManagementTools
+
+
+
+
 #variables
 $name_extend = "name"
 $1er_addr = "192.168.0.10"
 $last_addr = "192.168.0.250"
+$domain = "contoso.com"
 $mask = "255.255.255.0"
 $scope_ID = "192.168.0.0"
 $router = "192.168.0.254"
@@ -15,9 +22,12 @@ $router = "192.168.0.254"
 Add-DhcpServerv4Scope -Name "$name_extend" -StartRange "$1er_addr" -EndRange "$last_addr" -SubnetMask "$mask" -State InActive
 
 #Add option(s) for extend (gateway, expiration_bail):
+Set-DhcpServerv4OptionValue -DnsDomain "$domain"
 Set-DhcpServerv4OptionValue -ScopeId "$scope_ID" -Router "$router"
 Set-DhcpServerv4scope -ScopeId "$scope_ID" -LeaseDuration "8.00:00:00"
 Set-DhcpServerv4OptionValue -ScopeId "$scope_ID" -OptionId 6 -Value "1.1.1.1","1.1.1.2","1.1.1.3"
+
+
 
 
 #Active extend :
